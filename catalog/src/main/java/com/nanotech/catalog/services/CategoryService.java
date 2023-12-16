54186cd4957798e94a7a99531b0095c0612758d2
JanuarioAznick <januario-jose@outlook.com>
2023-12-16 04:12:33 +0200

    H2 database

---
+++ catalog/src/main/java/com/nanotech/catalog/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.nanotech.catalog.services;
+
+import com.nanotech.catalog.entities.Category;
+import com.nanotech.catalog.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll(){
+        return repository.findAll();
+    }
+
+}
