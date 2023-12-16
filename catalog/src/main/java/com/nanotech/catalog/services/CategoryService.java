61b0c86868ecd21cb3ed97fc2716b21eabb49b6c
JanuarioAznick <januario-jose@outlook.com>
2023-12-16 17:41:02 +0200

    DTO

--- catalog/src/main/java/com/nanotech/catalog/services/CategoryService.java
+++ catalog/src/main/java/com/nanotech/catalog/services/CategoryService.java
@@ -1,12 +1,15 @@
 package com.nanotech.catalog.services;
 
+import com.nanotech.catalog.dto.CategoryDTO;
 import com.nanotech.catalog.entities.Category;
 import com.nanotech.catalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -15,8 +18,8 @@ public class CategoryService {
     private CategoryRepository repository;
 
     @Transactional(readOnly = true)
-    public List<Category> findAll(){
-        return repository.findAll();
+    public List<CategoryDTO> findAll(){
+        List<Category> list = repository.findAll();
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
     }
-
 }
